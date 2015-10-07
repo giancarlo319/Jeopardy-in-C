d1075e8be7a2d2ef8ec9faa8029e03ed18d5f0df
acrodia <statictime@hotmail.com>
2015-10-07 12:44:40 -0400

    Adding player changes

--- players.c
+++ players.c
@@ -10,14 +10,33 @@
 #include <string.h>
 #include "players.h"
 
+/*
+    // Player struct for each player
+    typedef struct {
+    char name[MAX_LEN];
+    int score;
+} player;   
+ */
+
 // Returns true if the player name matches one of the existing players
 bool player_exists(player *players, char *name)
 {
+    if (*players.name == *name)
+    {
+        return true;
+    }
     return false;
 }
 
 // Updates the score for that player given their name
-void update_score(player *players, char *name, int score)
+void update_score(player *players, int num_players, char *name, int score)
 {
     
+    for(int i = 1; int <= num_players; i++)
+    {
+        if (*players[i].name == *name)
+        {
+            *players[i].score = score; 
+        }       
+    }
 }
