a85a97b7c5628eb2a722745e38d4fe8948f2715f
Elias Amal <elias.amal@uoit.net>
2015-10-13 21:41:06 -0400

    modified initialize and display_categories

--- questions.c
+++ questions.c
@@ -14,89 +14,103 @@
 void initialize_game(void)
 {
     
-    question questions[0].category = "geography"
-	question questions[0].question = "This is the capital city of Hungary"
-	question questions[0].answer = "Budapest"
-	question questions[0].value = 200
-	question questions[0].answered = false
+	strcpy(questions[0].category, "geography");
+	strcpy(questions[0].question, "This is the capital city of Hungary");
+	strcpy(questions[0].answer, "Budapest");
+	questions[0].value = 200;
+	questions[0].answered = false;
 
-	question questions[1].category = "geography"
-	question questions[1].question = "Jordan is located in this continent"
-	question questions[1].answer = "Asia"
-	question questions[1].value = 400
-	question questions[1].answered = false
+	strcpy(questions[1].category, "geography");
+	strcpy(questions[1].question, "Jordan is located in this continent");
+	strcpy(questions[1].answer, "Asia");
+	questions[1].value = 400;
+	questions[1].answered = false;
 
-	question questions[2].category = "geography"
-	question questions[2].question = "This country is bordered by Russia and Sweden"
-	question questions[2].answer = "Finland"
-	question questions[2].value = 600
-	question questions[2].answered = false
+	strcpy(questions[2].category, "geography");
+	strcpy(questions[2].question, "This country is bordered by Russia and Sweden");
+	strcpy(questions[2].answer, "Finland");
+	questions[2].value = 600;
+	questions[2].answered = false;
 
-	question questions[3].category = "geography"
-	question questions[3].question = "This country has the largest number of universities"
-	question questions[3].answer = "India"
-	question questions[3].value = 800
-	question questions[3].answered = false
-
-
-
-
-	question questions[4].category = "science"
-	question questions[4].question = "In this part of the cell cycle, chromosomes in a cell nucleus are separated into two identical sets of chromosomes"
-	question questions[4].answer = "Mitosis"
-	question questions[4].value = 200
-	question questions[4].answered = false
-
-	question questions[5].category = "science"
-	question questions[5].question = "Number of planets in our solar system"
-	question questions[5].answer = "8"
-	question questions[5].value = 400
-	question questions[5].answered = false
-
-	question questions[6].category = "science"
-	question questions[6].question = "This is the chemical formula for hydrogen peroxide"
-	question questions[6].answer = "H2O2"
-	question questions[6].value = 600
-	question questions[6].answered = false
-
-	question questions[7].category = "science"
-	question questions[7].question = "Type of energy that a body possesses by virtue of being in motion"
-	question questions[7].answer = "Kinetic"
-	question questions[7].value = 800
-	question questions[7].answered = false
+	strcpy(questions[3].category, "geography");
+	strcpy(questions[3].question, "This country has the largest number of universities");
+	strcpy(questions[3].answer, "India");
+	questions[3].value = 800;
+	questions[3].answered = false;
 
 
 
 
-	question questions[8].category = "computer networks"
-	question questions[8].question = "The Open System Interconnection model has this many layers"
-	question questions[8].answer = "7"
-	question questions[8].value = 200
-	question questions[8].answered = false
+	strcpy(questions[4].category, "science");
+	strcpy(questions[4].question, "In this part of the cell cycle, chromosomes in a cell nucleus are separated into two identical sets of chromosomes");
+	strcpy(questions[4].answer, "Mitosis");
+	questions[4].value = 200;
+	questions[4].answered = false;
 
-	question questions[9].category = "computer networks"
-	question questions[9].question = "This device forwards data packets between computer networks"
-	question questions[9].answer = "Router"
-	question questions[9].value = 400
-	question questions[9].answered = false
+	strcpy(questions[5].category, "science");
+	strcpy(questions[5].question, "Number of planets in our solar system");
+	strcpy(questions[5].answer, "8");
+	questions[5].value = 400;
+	questions[5].answered = false;
 
-	question questions[10].category = "computer networks"
-	question questions[10].question = "Frames from one LAN can be transmitted to another LAN via this device"
-	question questions[10].answer = "Bridge"
-	question questions[10].value = 600
-	question questions[10].answered = false
+	strcpy(questions[6].category, "science");
+	strcpy(questions[6].question, "This is the chemical formula for hydrogen peroxide");
+	strcpy(questions[6].answer, "H2O2");
+	questions[6].value = 600;
+	questions[6].answered = false;
 
-	question questions[11].category = "computer networks"
-	question questions[11].question = "Number of hosts attached to each of the local area networks at your site"
-	question questions[11].answer = "254"
-	question questions[11].value = 800
-	question questions[11].answered = false
+	strcpy(questions[7].category, "science");
+	strcpy(questions[7].question, "Type of energy that a body possesses by virtue of being in motion");
+	strcpy(questions[7].answer, "Kinetic");
+	questions[7].value = 800;
+	questions[7].answered = false;
+
+
+
+
+	strcpy(questions[8].category, "computer networks");
+	strcpy(questions[8].question, "The Open System Interconnection model has this many layers");
+	strcpy(questions[8].answer, "7");
+	questions[8].value = 200;
+	questions[8].answered = false;
+
+	strcpy(questions[9].category, "computer networks");
+	strcpy(questions[9].question, "This device forwards data packets between computer networks");
+	strcpy(questions[9].answer, "Router");
+	questions[9].value = 400;
+	questions[9].answered = false;
+
+	strcpy(questions[10].category, "computer networks");
+	strcpy(questions[10].question, "Frames from one LAN can be transmitted to another LAN via this device");
+	strcpy(questions[10].answer, "Bridge");
+	questions[10].value = 600;
+	questions[10].answered = false;
+
+	strcpy(questions[11].category, "computer networks");
+	strcpy(questions[11].question, "Number of hosts attached to each of the local area networks at your site");
+	strcpy(questions[11].answer, "254");
+	questions[11].value = 800;
+	questions[11].answered = false;
 
 }
 
 // Displays each of the remaining categories and question dollar values that have not been answered
 void display_categories(void)
 {
+
+	char v[12][20];
+
+	for (int i = 0; i < 12; i++) {
+		
+		if (questions[i].answered == false) {
+			
+			snprintf(v[i], 20, "%d", questions[i].value);
+			
+		}
+		
+		
+	}
+
 
 	char c1[] = categories[0];
 	char c2[] = categories[1];
